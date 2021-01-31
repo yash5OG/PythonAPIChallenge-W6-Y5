48d9d4bf3d10b319c8452400c86dab0a378946d8
Yash5 <yashalphabetagamma@gmail.com>
2021-01-30 21:51:16 -0800

    To hide API Keys

--- VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "9e04adce039f8575940ab491cb24b547"
-
-# Google API Key
-g_key = "AIzaSyAg4NMbiwZ_QvaH45YwmzCykZuWuSvo-Bc"
--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "9e04adce039f8575940ab491cb24b547"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"

867ebded7471acc27d1f09181d2b81029d56c29b
Yash5 <yashalphabetagamma@gmail.com>
2021-01-30 20:48:00 -0800

    Submission commit

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "Insert your own key"
+
+# Google API Key
+g_key = "Insert your own key"
--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9e04adce039f8575940ab491cb24b547"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
